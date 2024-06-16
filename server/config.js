6efc61141197099f404e887de24afa8a7085cd89
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2024-06-16 12:58:22 +0300

    fix production db

--- server/config.js
+++ server/config.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-export const MONGODB_URI = process.env.MONGODB_URI
+// export const MONGODB_URI = process.env.MONGODB_URI
+export const MONGODB_URI =
+  "mongodb+srv://alexurluescu23:12345@cluster0.pifc7s6.mongodb.net/";
 
-export const PORT = process.env.PORT || 4000
-
+// export const PORT = process.env.PORT || 4000;
+export const PORT = 3001 || 4000;
